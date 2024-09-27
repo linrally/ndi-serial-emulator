e13fa1a5aace91e7d9aaa90853673430273f7446
rally.lin@duke.edu <rl326@duke.edu>
2024-09-27 01:06:19 -0400

    Pose loading is now port handle specific

--- src/serialmanager.py
+++ src/serialmanager.py
@@ -1,6 +1,7 @@
 import serial
 import time
 from crc import CRC
+import re
 
 class SerialManager:
     def __init__(self, port_name):
@@ -54,3 +55,32 @@ class SerialManager:
             except UnicodeDecodeError:
                 pr = "[0x" + b.hex() + "]"
             print(pr, end="")
+    
+    def parse_rx(self): 
+        # TODO: CLEAN UP
+        if not self.rx_bytes.endswith(b'\r'):
+            raise RuntimeError("Expected \r at end of rx!")
+
+        command_match = re.match(rb'^[A-Za-z]+[: ]', self.rx_bytes)
+        if command_match is None:
+            raise ValueError(f"Error parsing command from '{self.rx_bytes}'!")
+
+        separator_idx = command_match.end()-1
+        separator = self.rx_bytes[separator_idx:separator_idx+1]
+        command = self.rx_bytes[:separator_idx].decode(encoding="utf-8", errors="strict")
+
+        if separator == b':': # end = 4-byte CRC (ASCII) + 1-byte \r
+            body_end_idx = len(self.rx_bytes) - 5
+            rx_body_bytes, rx_crc_ascii = self.rx_bytes[:body_end_idx], self.rx_bytes[body_end_idx:-1]
+            rx_crc_int = int(rx_crc_ascii, 16)
+            calc_crc_int = CRC.calc_crc16_int(rx_body_bytes)
+            if rx_crc_int != calc_crc_int:
+                raise ValueError(f"Received CRC 0x{rx_crc_int:X} doesn't match calculated CRC 0x{calc_crc_int:X} for message \"{rx_bytes}\"")
+        elif separator == b' ': # end = 1-byte \r (NO CRC)
+            body_end_idx = len(self.rx_bytes) - 1
+        else:
+            raise RuntimeError(f"Expected separator to either be ':' or ' ', but got '{separator}'!")
+
+        args = self.rx_bytes[separator_idx + 1:body_end_idx].decode(encoding="utf-8", errors="strict")
+
+        return command, args, rx_crc_int
